90db5a899f4ef3676183c6b58f33303135bb7898
Faraz <farazpourebrahimh@gmail.com>
2025-01-14 21:35:33 +0330

    [ADD] result section

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,14 @@
 import SearchBox from "@/components/SearchBox/SearchBox";
 
-import TourTypeFilter from "@/app/search/components/TourTypeFilter/TourTypeFilter";
-
 import styles from "./page.module.css";
+
+import TourTypeFilter from "@/app/search/components/TourTypeFilter/TourTypeFilter";
 import PriceRangeFilter from "@/app/search/components/PriceRangeFilter/PriceRangeFilter";
 import DurationFilter from "@/app/search/components/DurationFilter/DurationFilter";
 import TourGuideFilter from "@/app/search/components/TourGuideFilter/TourGuideFilter";
+import Card from "@/app/search/components/Card/Card";
+import { mockTours } from "@/mocks/mockTours";
+import Sort from "@/app/search/components/Sort/Sort";
 
 export default function page() {
   return (
@@ -16,7 +19,27 @@ export default function page() {
         <DurationFilter />
         <TourGuideFilter />
       </div>
-      <div className={styles.main}></div>
+      <div className={styles.main}>
+        <SearchBox />
+        <div className={styles["result-header"]}>
+          <p className={styles.count}>{mockTours.length} نتیجه پیدا شد</p>
+          <Sort />
+        </div>
+        <div className={styles.result}>
+          {mockTours.map((item) => (
+            <Card
+              key={item.id}
+              title={item.title}
+              location={item.location}
+              price={item.price}
+              duration={item.duration}
+              guideAvailable={item.guideAvailable}
+              type={item.type}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
