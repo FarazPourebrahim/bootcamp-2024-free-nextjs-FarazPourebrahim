5f5dfe2f9eb3b1178964bc120f5c07c60317564e
Faraz <farazpourebrahimh@gmail.com>
2025-01-17 22:04:12 +0330

    [ADD] additional info to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import Hero from "@/components/Hero/Hero";
 import Recommended from "@/components/Recommended/Recommended";
 
 import styles from "./page.module.css";
+import AdditionalInfo from "@/components/AdditionalInfo/AdditionalInfo";
 
 export default function Home(): ReactElement {
   return (
     <div className={styles.home}>
       <Hero />
       <Recommended />
+      <AdditionalInfo />
     </div>
   );
 }
