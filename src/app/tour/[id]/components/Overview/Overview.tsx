689148ed0130d3841fe460e5fc59e2d5ed091b43
Faraz <farazpourebrahimh@gmail.com>
2025-01-27 13:05:00 +0330

    [ADD] overview component

---
+++ src/app/tour/[id]/components/Overview/Overview.tsx
@@ -0,0 +1,52 @@
+import React, { ReactElement } from "react";
+import styles from "./Overview.module.css";
+import { mockTours } from "@/mocks/mockTours";
+
+type Props = {
+  tourId: number;
+};
+
+export default function Overview({ tourId }: Props): ReactElement {
+  const tour = mockTours.find((tour) => tour.id === tourId);
+
+  const tourTypes = [
+    { value: "Cultural", label: "فرهنگی" },
+    { value: "Adventure", label: "ماجراجویی" },
+    { value: "Luxury", label: "لوکس" },
+    { value: "Family", label: "خانوادگی" },
+    { value: "Nature", label: "طبیعت‌گردی" },
+    { value: "Historical", label: "تاریخی" },
+    { value: "Sports", label: "ورزشی" },
+    { value: "Relaxation", label: "آرامش" },
+  ];
+
+  const tourTypeLabel = tourTypes.find(
+    (type) => type.value === tour?.type,
+  )?.label;
+
+  return (
+    <div className={styles.section}>
+      <h2 className={styles.title}>{tour?.title}</h2>
+      <p className={styles.detail}>
+        <span className={styles.label}>موقعیت: </span>
+        {tour?.location}
+      </p>
+      <p className={styles.detail}>
+        <span className={styles.label}>قیمت: </span>
+        {tour?.price.toLocaleString("fa-IR")} تومان
+      </p>
+      <p className={styles.detail}>
+        <span className={styles.label}>مدت زمان: </span>
+        {tour?.duration} روز
+      </p>
+      <p className={styles.detail}>
+        <span className={styles.label}>راهنما موجود: </span>
+        {tour?.guideAvailable ? "بله" : "خیر"}
+      </p>
+      <p className={styles.detail}>
+        <span className={styles.label}>نوع: </span>
+        {tourTypeLabel}
+      </p>
+    </div>
+  );
+}
