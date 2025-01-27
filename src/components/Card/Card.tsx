27a5419eabb6ad6ed0bfe61de9bd0f3907f51072
Faraz <farazpourebrahimh@gmail.com>
2025-01-27 14:13:17 +0330

    [ADD] link to cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,6 @@
 import styles from "./Card.module.css";
 import Image from "next/image";
+import Link from "next/link";
 import { MockTour } from "@/mocks/mockTours";
 
 const typeToPersian: Record<MockTour["type"], string> = {
@@ -14,28 +15,30 @@ const typeToPersian: Record<MockTour["type"], string> = {
 };
 
 export default function Card({ tour }: { tour: MockTour }) {
-  const { title, location, price, duration, guideAvailable, type, image } =
+  const { id, title, location, price, duration, guideAvailable, type, image } =
     tour;
 
   return (
-    <div className={styles.card}>
-      <Image
-        src={image}
-        alt={title}
-        className={styles.image}
-        width={400}
-        height={400}
-      />
-      <div className={styles.content}>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.location}>{location}</p>
-        <p className={styles.details}>
-          <span>{duration} روز</span>
-          <span>{guideAvailable ? "با راهنما" : "بدون راهنما"}</span>
-        </p>
-        <p className={styles.type}>{typeToPersian[type]}</p>
-        <p className={styles.price}>{price.toLocaleString()} تومان</p>
+    <Link href={`/tour/${id}`} className={styles.cardLink}>
+      <div className={styles.card}>
+        <Image
+          src={image}
+          alt={title}
+          className={styles.image}
+          width={400}
+          height={400}
+        />
+        <div className={styles.content}>
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.location}>{location}</p>
+          <p className={styles.details}>
+            <span>{duration} روز</span>
+            <span>{guideAvailable ? "با راهنما" : "بدون راهنما"}</span>
+          </p>
+          <p className={styles.type}>{typeToPersian[type]}</p>
+          <p className={styles.price}>{price.toLocaleString()} تومان</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
