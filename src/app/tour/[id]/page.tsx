5693c7d6d5ff6711bca99b14c69e75dda574feb3
Faraz <farazpourebrahimh@gmail.com>
2025-01-27 13:04:01 +0330

    [ADD] tour detail page layout

--- src/app/tour/[id]/page.tsx
+++ src/app/tour/[id]/page.tsx
@@ -1,6 +1,11 @@
 import { ReactElement } from "react";
 import { MockTour, mockTours } from "@/mocks/mockTours";
 import { notFound } from "next/navigation";
+import styles from "./page.module.css";
+import Card from "@/components/Card/Card";
+import Image from "next/image";
+import ReturnButton from "@/components/ReturnButton/ReturnButton";
+import TourDetails from "@/app/tour/[id]/components/TourDetails/TourDetails";
 
 type Props = {
   params: { id: string };
@@ -15,5 +20,37 @@ export default function page({ params }: Props): ReactElement {
     return notFound();
   }
 
-  return <p>Hello{tour?.title}</p>;
+  //TEMP
+  const getRandomIndexes = (count: number, max: number): number[] => {
+    const indexes = new Set<number>();
+    while (indexes.size < count) {
+      const randomIndex = Math.floor(Math.random() * max);
+      indexes.add(randomIndex);
+    }
+    return Array.from(indexes);
+  };
+
+  const randomIndexes = getRandomIndexes(3, mockTours.length);
+  const randomTours = randomIndexes.map((index) => mockTours[index]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.recommended}>
+        <h3 className={styles["recommended-header"]}>تورهای مشابه</h3>
+        {randomTours.map((tour) => (
+          <Card key={tour.id} tour={tour} />
+        ))}
+      </div>
+      <div className={styles.main}>
+        <div className={styles["main-header"]}>
+          <h2 className={styles.title}>{tour.title}</h2>
+          <ReturnButton path={"/search"}>بازگشت</ReturnButton>
+        </div>
+        <div className={styles.image}>
+          <Image src={tour.image} alt={tour.title} width={600} height={400} />
+        </div>
+        <TourDetails tourId={Number(params.id)} />
+      </div>
+    </div>
+  );
 }
