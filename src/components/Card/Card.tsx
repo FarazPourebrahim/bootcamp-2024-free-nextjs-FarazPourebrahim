0e61a9b293bc3355eba6d80260751d017ec9e065
Faraz Pourebrahim <127433282+FarazPourebrahim@users.noreply.github.com>
2025-01-28 10:35:56 +0330

    [FIX] add Link to card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 import styles from "./Card.module.css";
 import Image from "next/image";
-import { useRouter } from "next/navigation"; // Use `next/navigation` for routing in Next.js 13+
+import Link from "next/link";
 import { MockTour } from "@/mocks/mockTours";
 
 const typeToPersian: Record<MockTour["type"], string> = {
@@ -19,31 +17,28 @@ const typeToPersian: Record<MockTour["type"], string> = {
 export default function Card({ tour }: { tour: MockTour }) {
   const { id, title, location, price, duration, guideAvailable, type, image } =
     tour;
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(`/tour/${id}`);
-  };
 
   return (
-    <div className={styles.card} onClick={handleClick}>
-      <Image
-        src={image}
-        alt={title}
-        className={styles.image}
-        width={400}
-        height={400}
-      />
-      <div className={styles.content}>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.location}>{location}</p>
-        <p className={styles.details}>
-          <span>{duration} روز</span>
-          <span>{guideAvailable ? "با راهنما" : "بدون راهنما"}</span>
-        </p>
-        <p className={styles.type}>{typeToPersian[type]}</p>
-        <p className={styles.price}>{price.toLocaleString()} تومان</p>
+    <Link href={`/tour/${id}`} className={styles.cardLink}>
+      <div className={styles.card}>
+        <Image
+          src={image}
+          alt={title}
+          className={styles.image}
+          width={400}
+          height={400}
+        />
+        <div className={styles.content}>
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.location}>{location}</p>
+          <p className={styles.details}>
+            <span>{duration} روز</span>
+            <span>{guideAvailable ? "با راهنما" : "بدون راهنما"}</span>
+          </p>
+          <p className={styles.type}>{typeToPersian[type]}</p>
+          <p className={styles.price}>{price.toLocaleString()} تومان</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
