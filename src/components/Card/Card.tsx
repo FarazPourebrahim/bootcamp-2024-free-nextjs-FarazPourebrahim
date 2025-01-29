195d224746a405e881646c92f00337f88f264684
Faraz <farazpourebrahimh@gmail.com>
2025-01-29 21:19:49 +0330

    [FIX] Link messing up styles problem

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -19,25 +19,23 @@ export default function Card({ tour }: { tour: MockTour }) {
     tour;
 
   return (
-    <Link href={`/tour/${id}`} className={styles.cardLink}>
-      <div className={styles.card}>
-        <Image
-          src={image}
-          alt={title}
-          className={styles.image}
-          width={400}
-          height={400}
-        />
-        <div className={styles.content}>
-          <h3 className={styles.title}>{title}</h3>
-          <p className={styles.location}>{location}</p>
-          <p className={styles.details}>
-            <span>{duration} روز</span>
-            <span>{guideAvailable ? "با راهنما" : "بدون راهنما"}</span>
-          </p>
-          <p className={styles.type}>{typeToPersian[type]}</p>
-          <p className={styles.price}>{price.toLocaleString()} تومان</p>
-        </div>
+    <Link href={`/tour/${id}`} className={styles.card}>
+      <Image
+        src={image}
+        alt={title}
+        className={styles.image}
+        width={400}
+        height={400}
+      />
+      <div className={styles.content}>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.location}>{location}</p>
+        <p className={styles.details}>
+          <span>{duration} روز</span>
+          <span>{guideAvailable ? "با راهنما" : "بدون راهنما"}</span>
+        </p>
+        <p className={styles.type}>{typeToPersian[type]}</p>
+        <p className={styles.price}>{price.toLocaleString()} تومان</p>
       </div>
     </Link>
   );
